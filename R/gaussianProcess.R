5f10c96027027b007267c03f50d2525eb6bc46de
Eli Ben-Michael <ebenmichael@berkeley.edu>
2017-02-20 23:01:35 -0800

    Added amplitude parameter for kernels

--- R/gaussianProcess.R
+++ R/gaussianProcess.R
@@ -15,19 +15,21 @@ zeroFunction <- function(x) {
 #' @param kernel The covariance kernel of the process, defaults to rbf
 #' @param noiseVar The variance of the noise around the function
 #' @param scale The scale of the kernel, defaults to 1
-#' @param order The roder of the kernel, defaults to NULL
+#' @param order The order of the kernel, defaults to NULL
+#' @param amplitude A parameter which is multiplied by the kernel, defaults to 1
 #' @return A gaussianProcess with these options
 gaussianProcess <- function(X, y, meanFunc=zeroFunction, kernel=rbf,
-                            noiseVar=1, scale=1, order=NULL) {
+                            noiseVar=1, scale=1, order=NULL, amplitude=1) {
     # initialize the list
     gaussianProcess <- list()
     # set the kernel
     if(identical(kernel, rbf)) {
-        gaussianProcess$kernel <- pryr::partial(kernel, scale=scale)
+        gaussianProcess$kernel <- pryr::partial(kernel, scale=scale,
+                                                amplitude=amplitude)
     }
     else if(identical(kernel, matern)) {
         gaussianProcess$kernel <- pryr::partial(kernel, scale=scale,
-                                                order=order)
+                                                order=order, amplitude=amplitude)
     }
     else {
         stop("Kernel must be rbf or matern. Working to fix this")
--- R/kernels.R
+++ R/kernels.R
@@ -5,15 +5,16 @@
 #' @param X An n x d matrix
 #' @param Y An n x d matrix
 #' @param scale The scale parameter, defaults to 1
+#' @param amplitude A parameter which is multiplied by the kernel, defaults to 1
 #' @return An n x n kernel matrix formed from \code{X} and \code{Y}
-rbf <- function(X, Y, scale=1){
+rbf <- function(X, Y, scale=1, amplitude=1){
     # pairwise distances
     n <- dim(X)[1]
     distMat <- matrix(proxy::dist(X, Y), nrow=dim(X)[1])
     # square to get the squared norm
     distMat <- distMat ^ 2
     # Get the squared exponential kernel matrix
-    K <- exp(- distMat / (2 * scale ^ 2))
+    K <- exp(- distMat / (2 * scale ^ 2)) * amplitude
     return(K)
     
 }
@@ -24,16 +25,18 @@ rbf <- function(X, Y, scale=1){
 #' @param Y An n x d matrix
 #' @param scale The scale parameter, defaults to 1
 #' @param order whether to compute a 3/2 or 5/2 kernel, defaults to 5/2
+#' @param amplitude A parameter which is multiplied by the kernel, defaults to 1
 #' @return An n x n kernel matrix formed from \code{X} and \code{Y}
-matern <- function(X, Y, scale=1, order=5/2){
+matern <- function(X, Y, scale=1, order=5/2, amplitude=1){
     # compute pairwise distances
     distMat <- matrix(proxy::dist(X, Y), nrow=dim(X)[1])
     if(order == 3/2){
         K <- (1 + sqrt(3) * distMat / scale) * exp(- sqrt(3) * distMat / scale)
+        K <- K * amplitude
     }
     else if(order == 5/2) {
         K <- 1 + sqrt(5) * distMat / scale + 5 * distMat ^ 2 / (3 * scale ^ 2)
-        K <- K * exp(-sqrt(5) / scale)
+        K <- K * exp(-sqrt(5) / scale) * amplitude
     }
     else{
         stop("Only Matern 3/2 and 5/2 are supported")
